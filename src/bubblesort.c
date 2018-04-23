c1780a8da6728d7f3dc37e51f4cc8bc6b15e16e1
BuildTools <gxm262@case.edu>
2018-04-23 15:59:31 -0400

    Made some changes to bubble sort

--- src/bubblesort.c
+++ src/bubblesort.c
@@ -3,23 +3,38 @@ Bubblesort. Inspired by the algorithm available
 at URL : https://www.geeksforgeeks.org/bubble-sort*/
 
 #include <stdio.h>
- 
+
+int size;
+
+void swap(int i, int j);
+
 // A function to implement bubble sort
 void bubbleSort(int arr[], int n){
    int i, j;
-   for (i = 0; i < n-1; i++){    
-       // Last i elements are already in place   
+   size = n;
+   for (i = 0; i < n-1; i++){
+       // Last i elements are already in place
        for (j = 0; j < n-i-1; j++){
            if (arr[j] > arr[j+1]){
-              swap(&arr[j], &arr[j+1]);
+              swap(j, j+1);
 			  int temp = arr[j];
 			  arr[j] = arr[j+1];
 			  arr[j+1] = temp;
 		   }
 		}
-   }	
+   }
 }
- 
+
+
+// Swaps the two elements at positions i and j in the array
+void swap (int arr[],int i, int j) {
+  if (i < j && i >= 0 && j<size){
+    int temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+}
+
 // Driver program to test above functions
 int main()
 {
