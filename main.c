fac693633afaeb7906206cacaff00365cc70ef9b
noahhoupt <36174737+noahhoupt@users.noreply.github.com>
2018-04-23 16:28:26 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+//Main function that calls the sorting algorithms
+//Noah Houpt and Guy Marino
+
+
+/*The purpose of this script is to 
+implement the sorting algorithms with various parameters
+the parameters we are testing are:
+
+Number of threads: 1, 2, 4, 8, 16, 32, 64
+
+Length of Array: 10, 20, 50, 100, 200, 500,
+1000, 2000, 5000, 10 000, 20 000, 50, 000, 100 000, 200 000,
+500 000, 1 000 000, 2 000 000, 5 000 000, 10 000 000, 20 000 000
+50 000 000, 100 000 000, 200 000 000, and 500 000 000
+
+Different Sorting Algorithms: Bubble, Quick, Merge
+
+We will be timing each of these parameters in order to 
+check the data and obtain a result of the experiment
+
+The fucntions we are working with are:
+
+quicksort(int[] array, int threadCount);
+mergesort(int[] array, int threadCount);
+bubblesort(int[] array, int threadCount);*/
+main(){
+	
+
+}
