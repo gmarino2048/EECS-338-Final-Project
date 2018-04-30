c072ef59bd3e8963c147fe707d7eb67ea9263d73
BuildTools <gxm262@case.edu>
2018-04-30 18:18:12 -0400

    Some stuff

--- src/bubblesort.c
+++ src/bubblesort.c
@@ -84,7 +84,10 @@ void bubblesort(int *arr, int threads, int size){
 
    // Instantiate the pthreads
    for (int i = 0; i < actualThreads; i++) {
-     argsList[i] = {arr, i * 2, size};
+     struct Args argsList[i];
+     argsList[i].arr = arr;
+     argsList[i].position = i*2;
+     argsList[i].size = size;
      pthread_create(&pthreads[i], &attributes[i], bubble, (void *) &argsList[i]);
    }
 
