d068392f2e5bd115ceab0e6bfb21f03490ddb974
BuildTools <gxm262@case.edu>
2018-04-30 13:49:59 -0400

    Quicksort maybe complete???

--- src/quicksort.c
+++ src/quicksort.c
@@ -3,48 +3,104 @@
 # include <stdlib.h>
 # include <pthread.h>
 
-int index;
+int threadCount
 int MAX_THREADS;
-sem_t semaphore;
 
-struct params {
-  pthread_t pthreads[];
-  pthread_attr attributes[];
-  int list[];
+pthread_t *pthreads;
+pthread_attr *attributes;
+
+sem_t mutex;
+
+struct Args {
+  int arr;
   int start;
   int stop;
 };
 
-void swap (int list[], int index1, int index2){
- int temp = list[index1];
- list[index1] = list[index2];
- list[index2] = temp;
-}
+void *quicksort_setup(void *args);
+int partition(int arr[], int start, int stop);
+void swap(int arr[], int index1, int index2);
 
-void partition (int arr[], int partitionIndex){
- //This method must be written, it will partition data at a partition index 
-}
-
-
-int quicksort_setup (int list[], int maxThreads){
+int *quicksort(int *arr, int threads, int size){
   if (maxThreads == 0){
     printf("Cannot run on 0 threads\n", stderr);
-    return -1;
+    exit(-1);
   }
-
+  threadCount = 0;
   MAX_THREADS = maxThreads;
 
-  if (sem_init(&semaphore, 0, 1) != 0){
-    printf("Failed to instantiate semaphore\n", stderr);
-    return -1;
+  pthread_t tempThreads[maxThreads];
+  pthread_attr tempAttributes[maxThreads];
+
+  pthreads = tempThreads;
+  attributes = tempAttributes;
+
+  if (sem_init(&mutex, 1, 1) < 0){
+    printf ("Could not initialize semaphore", stderr);
+    exit(-1);
   }
 
-  pthread_t pthreads[maxThreads];
-  pthread_attr attributes[maxThreads];
+  struct Args initial;
 
+  initial.arr = arr;
+  initial.start = 0;
+  initial.stop = size;
 
+  pthread_create(pthreads[0], attributes[0], quicksort_setup, (void *)initial);
 }
 
-int[] quicksort(int arr[], int threads, int size){
- //this method still must be completed, serves as a wrapper class 
+void *quicksort_setup (void *args){
+  struct Args arguments = (struct Args *) args;
+
+  if (args.start < (args.stop - 1)){
+    int pivot = partition(args.arr, args.start, args.stop);
+
+    sem_wait(&mutex);
+    if (threadCount < (maxThreads - 1)){
+      threadCount++;
+
+      struct Args right = {args.arr, pivot, args.stop};
+      pthread_create(pthreads[threadCount])
+
+      sem_post(&mutex);
+
+      struct Args left = {args.arr, args.start, pivot};
+      *quicksort_setup((void *)left);
+    }
+    else {
+      sem_post(&mutex);
+
+      struct Args left = {args.arr, args.start, pivot};
+      *quicksort_setup((void *)left);
+
+      struct Args right = {args.arr, pivot, args.stop};
+      *quicksort_setup((void *)right);
+    }
+  }
+}
+
+int partition (int *arr, int start, int stop){
+ // Partition a sublist of arr using the start and stop indices
+ int low = start;
+ int high = stop - 1;
+
+ int pivot = arr[high];
+
+ int i = low - 1;
+
+ for (int j = low; j < high; j++){
+   if (arr[j] < pivot){
+     i++;
+     swap(arr, i, j);
+   }
+ }
+
+ swap(arr, i + 1, high);
+ return i + 1;
+}
+
+void swap (int *arr, int index1, int index2){
+ int temp = arr[index1];
+ arr[index1] = arr[index2];
+ arr[index2] = temp;
 }
