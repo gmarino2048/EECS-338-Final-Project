dbe4a1a87a1b62840a2c43a424a22bd813b37cae
BuildTools <gxm262@case.edu>
2018-04-30 21:05:21 -0400

    Lots of changes

--- src/bubblesort.c
+++ src/bubblesort.c
@@ -185,3 +185,9 @@ void *bubble (void *arguments){
   }
   pthread_exit(NULL);
 }
+
+void swap (int *arr, int index1, int index2){
+  int temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
