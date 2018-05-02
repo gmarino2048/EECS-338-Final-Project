9a8abeb431977caab23fb6729c93c423baea8d5a
noahhoupt <36174737+noahhoupt@users.noreply.github.com>
2018-05-01 21:27:07 -0400

    Update bubblesort.c

--- src/bubblesort.c
+++ src/bubblesort.c
@@ -54,7 +54,7 @@ int main (int argc, char* argv[]) {
   struct timeval start_time, stop_time, elapsed_time;
 
   for (int threads = 1; threads < 128; threads = threads *2){
-    for (int size = 1; size < 1000000; size = size *2){
+    for (int size = 1; size < 20000; size = size *2){
       int *arr = randomArray(size);
 
       gettimeofday(&start_time,NULL);
